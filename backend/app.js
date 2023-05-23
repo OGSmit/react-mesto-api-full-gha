6f832bf8bc4e6a74c4f7e3b3819a1fbe8a5f5384
Oleg Smitkevich <o.smitkevich@gmail.com>
2023-05-23 17:24:56 +0300

    Добавляет пакет Cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const errorHandler = require('./utils/errorHandler');
@@ -8,6 +9,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(cors());
 
 app.use(helmet());
 
